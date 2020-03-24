b0957872ac9c240e3bf3b4f83d2d0f0bd26546df
BS263 <sifathasnain@yahoo.com>
2020-03-24 12:39:59 +0600

    fixed file download

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+import urllib.request
+
+from flask import Flask, request, redirect, jsonify, send_from_directory, abort
 
 UPLOAD_FOLDER = 'uploads'
 
@@ -7,4 +10,28 @@ app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
+ALLOWED_EXTENSIONS = set(['png'])
 
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/file-serve/<path:filename>')
+def upload_file(filename):
+	# check if filename is empty
+	if filename == '':
+		resp = jsonify({'message' : 'File '})
+		resp.status_code = 400
+		return resp
+	if allowed_file(filename):
+		try:
+			return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+		except FileNotFoundError:
+			abort(404)
+	else:
+		resp = jsonify({'message' : 'Allowed file type is png'})
+		resp.status_code = 400
+		return resp
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
